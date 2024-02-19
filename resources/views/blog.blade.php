97ab8804afbe885aa8be32e30a402eb574fe8a58
HaPham2024 <sangwebsite1992.com>
2024-02-19 18:27:19 +0700

    bai-5

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -4,11 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Học lập trình laravel</title>
+    <title>Tin tức</title>
 </head>
 <body>
-    <h1>Học lập trình tại Unicode</h1>
-    <a href="{{ route('tin-tuc') }}">Trang chủ tin tức</a>
-
+    <h1>Tin tức</h1>
+    <a href="{{route('home')}}">Trang chủ</a>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,16 +32,58 @@ use App\Models\User;
 //     return 'phương thức put';
 // });
 
-Route::prefix('admin')->group(function(){
-    Route::get('unicode',function(){
-        return 'Phương thức Get của Path/unicode';
-    });
+// Route::prefix('admin')->group(function(){
+//     Route::get('unicode',function(){
+//         return 'Phương thức Get của Path/unicode';
+//     });
 
+//     Route::get('show-form',function(){
+//         return view('form');
+//     });
+
+//     Route::prefix('products')->group(function(){
+//         Route::get('/',function(){
+//             return 'Danh sach san pham';
+//         });
+//         Route::get('add',function(){
+//             return 'Thêm sản phẩm';
+//         });
+//         Route::get('edit',function(){
+//             return 'chỉnh sửa';
+//         });
+//     });
+// });
+
+
+// Route::get('/', function(){
+//     return view('home');
+// })->name('home');
+
+
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('/tin-tuc', [HomeController::class, 'getNew'])->name('tin-tuc');
+
+
+Route::redirect('unicode','show-form');
+
+Route::prefix('admin')->group(function(){
+    Route::get('tin-tuc/{slug}-{id}.html',function($slug=null,$id=null){
+        $content = 'phương thức put với tham số ';
+        $content.= 'id = '.$id.'</br>';
+        $content.= 'slug = '.$slug;
+        return $content;
+    })->where(
+        [
+            'slug'=>'[.+]',
+            'id' => '[0-9]+'
+        ]
+    )->name('admin.tintuc');
     Route::get('show-form',function(){
         return view('form');
     });
-
-    Route::prefix('products')->group(function(){
+    Route::prefix('products')->middleware('check.permission')->group(function(){
         Route::get('/',function(){
             return 'Danh sach san pham';
         });
