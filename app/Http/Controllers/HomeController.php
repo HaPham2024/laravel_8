b6ad1354e94dc1d6d55f944eb7ceb3052e009f69
HaPham2024 <sangwebsite1992.com>
2024-02-29 19:28:39 +0700

    bai-11

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,19 +6,20 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public $data = [];
     public function index()
     {
-        $title = "Hoc Lap Trinh";
-        $content = "Hoc Lap Trinh tai unicode";
-        // compact('title','content')
-        return view('home')->with([
-            'title' => $title,
-            'content' => $content
-        ]); // Load
-    }
-    public function getNews()
-    {
-        return 'Danh sách tin tức';
+        $this->data['welcome'] = 'Hoc lap trinh';
+        $this->data['content'] = '<h3>Chuong 1 : nhap mon</h3><p>Kien thuc 1</p><p>Kien thuc 2</p><p>Kien thuc 3</p>';
+        // $dataArr = [
+        //     'item1' => 'Value 1',
+        //     'item2' => 'Value 2',
+        //     'item3' => 'Value 3',
+        // ];
+        $this->data['number'] = 20;
+
+        return view('home', $this->data);
+
+        // return view('home', compact('dataArr'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Học lập trình laravel</title>
 </head>
+
 <body>
-    <header>
-        <h1>HEADER - UNICODE</h1>
-        <h2>@php
-            echo $title;
-        @endphp</h2>
-    </header>
-    <main>
-        <h1>NỘI DUNG - UNICODE</h1>
-    </main>
-    <footer>
-        <h1>FOOTER - UNICODE</h1>
-    </footer>
+    <h1>Trang chu</h1>
+    {{-- <h2>{{ $welcome }}</h2> --}}
+    {{-- <h2>?php echo request()->keyword; ?></h2> --}}
+    {{-- <h2>{!! $content !!}</h2> --}}
+
+    {{-- @for ($i = 1; $i <= 10; $i++)
+        <p>Phan tu thu : {{ $i }}</p>
+    @endfor --}}
+
+    {{-- @foreach ($dataArr as $key => $item)
+        <p>Phan tu: {{ $item }} - {{ $key }}</p>
+    @endforeach --}}
+
+    @switch($number)
+        @case(1)
+            <p>So thu nhat</p>
+        @break
+
+        @case(2)
+            <p>So thu hai</p>
+        @break
+
+        @default
+            <p>so con lai</p>
+    @endswitch
+
 </body>
+
 </html>
