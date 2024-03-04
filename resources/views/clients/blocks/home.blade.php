4c6039fe22fe07087caa351aa157d46db7bee478
HaPham2024 <sangwebsite1992.com>
2024-03-04 20:34:07 +0700

    bai 16

--- resources/views/clients/blocks/home.blade.php
+++ resources/views/clients/blocks/home.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.client')
-@section('titlet')
+
+@section('title')
     <h1>Trang chủ - </h1>
 @endsection
+
 @section('sidebar')
     @parent
     <h1>Home Sidebar</h1>
@@ -9,7 +11,18 @@
 
 @section('content')
     <h1> Trang chủ </h1>
+    <p>{{ \Carbon\Carbon::parse('2022-12-03 15:00:00')->format('Y-m-d H:i:s') }}</p>
+    <p>{{ \Carbon\Carbon::parse('2022-12-06 15:00:00')->format('Y-m-d H:i:s') }}</p>
+
+    @env('production')
+    <p>Môi trường production </p>
+    @elseenv('test')
+    <p>Không phải môi trường test</p>
+@else
+    <p>Không phải môi trường dev</p>
+    @endenv
 @endsection
 
 @section('css')
+    <!-- Additional CSS if needed -->
 @endsection
